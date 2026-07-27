beee4688fa9a11aaee579b4b499464e85cbbbc52
benlkadihamza <benlkadi.hamza@gmail.com>
2026-07-27 16:00:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,13 @@ class ProductionConfig(Config):
     db_url = os.environ.get('DATABASE_URL', '')
     if db_url.startswith('postgres://'):
         db_url = db_url.replace('postgres://', 'postgresql://', 1)
+
+    if db_url.startswith('postgresql://'):
+        try:
+            import psycopg2  # noqa
+        except Exception:
+            db_url = db_url.replace('postgresql://', 'postgresql+psycopg://', 1)
+
     SQLALCHEMY_DATABASE_URI = db_url or ('sqlite:///' + os.path.join(basedir, 'database', 'app.db'))
 
 
